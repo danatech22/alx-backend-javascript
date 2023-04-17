1634c4840386195bb662ca5de30c7660ba95a4c0
Shittu Tope <danatech22@gmail.com>
2023-04-17 08:50:04 -0700

    Update

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,27 +1,7 @@
 export default function createIteratorObject(report) {
-  let employeeIndex = 0;
-  let departmentIndex = 0;
-
-  const iterator = {
-    next() {
-      if (departmentIndex >= report.length) {
-        return { done: true };
-      }
-
-      const currentDepartment = report[departmentIndex];
-      const currentEmployee = currentDepartment.employees[employeeIndex];
-
-      if (employeeIndex < currentDepartment.employees.length - 1) {
-        employeeIndex++;
-      } else {
-        employeeIndex = 0;
-        departmentIndex++;
-      }
-
-      return { value: currentEmployee };
-    }
-  };
-
-  return iterator;
+  const result = [];
+  for (const idx of Object.values(report.allEmployees)) {
+    result.push(...idx);
+  }
+  return result;
 }
-
